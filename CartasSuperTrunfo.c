62155f6713d4fe793351808cd0ba676027959cce
maria05eduarda <eduarda14souza12@gmail.com>
2025-03-06 20:29:26 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int main () {
 
-    return 0;
-}
+        // Dando nome as variaveis e os seus valores.
+        char estado = 'A';
+        char codigo[50] = "A01";
+        char nome[50] = "Riodejaneiro";
+        int populacao = 6211223;
+        float area = 1.200;
+        float pib = 1.150;
+        int npt = 58;
+
+        printf("Ola! Boa tarde, aqui abaixo esta o cadastro da primeira carta!\n");
+        printf("Carta 1: \n");
+        printf("Estado: %c\n", estado);
+        printf("Codigo: %s\n", codigo);
+        printf("Nome da cidade: %s\n", nome);
+        printf("População: %d\n", populacao);
+        printf("Area em Km²: %.3f Km²\n", area);
+        printf("PIB: %.3f Milhoes\n", pib);
+        printf("Numero de Pontos Turisticos: %d\n", npt);
+
+        //Agora, vou utilizar as mesmas variaveis, entretanto, vou mudar os nomes significativos e os resultados.
+
+        char Estado = 'B';
+        char Codigo[50] = "B02";
+        char Nome[50] = "ceara";
+        int Populacao = 8.795;
+        float Area = 148.886;
+        float PIB = 1.000;
+        int NPT = 20;
+        //Aqui, nos nomes significativos, coloquei todos com no maximo 1 letra maiuscula, para ter uma diferença na variação no topo do codigo.
+
+        //Refazendo o Printf novamente.
+
+        printf("Agora, o cadastro da segunda carta!\n");
+        printf("Carta 2: \n");
+        printf("Estado: %c\n", Estado);
+        printf("Codigo: %s\n", Codigo);
+        printf("Nome da Cidade: %s\n", Nome);
+        printf("População: %d\n", Populacao);
+        printf("Area em Km²: %.2f Km²\n", Area);
+        printf("PIB: %.1f reais\n", PIB);
+        printf("Numero de Pontos Turisticos: %d\n", NPT);
+
+        printf ("Cadastro de duas cartas concluido, pode fechar o terminal!");
+
+        return 0;
+    }
